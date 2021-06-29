c4185fc5df29205ae320fcc4854c3a24dcf0dfcd
Davidxy9 <davidwilliamlima@hotmail.com>
2021-06-29 17:58:27 -0300

    outros testes css Summary

--- src/components/Summary/index.tsx
+++ src/components/Summary/index.tsx
@@ -1,40 +1,40 @@
 import storeImg2 from '../../assets/storeImg2.png';
 import clockImg from '../../assets/clock.png';
-import { Container, Content, Exemplo1, Exemplo2, Exemplo3, Exemplo4, Exemplo5 } from './styles';
+import carImg from '../../assets/carStore.png';
+import { Container, Content } from './styles';
 
 export function Summary() {
     return (
         <Container>
-        <img src={storeImg2} alt="Imagem da loja" />
-        <Exemplo2>
             <Content>
+            <img src={storeImg2} alt="Imagem da loja" />
+            <div>
                 <strong>Pet Friends Acessories</strong>
                 <img src={clockImg} alt="Imagem relógio" />
-                <h3>ABERTO AGORA</h3>
+            </div>
+            <h3>ABERTO AGORA</h3>
+            
+            <p>Avenida Rio Grande do Sul,1520,Estados | 58030-021 | João Pessoa - PB</p>
             </Content>
+            <div style={{
+                display: 'flex',
+                justifyContent: 'space-around'
+            }}>
+            <div style={{display: 'flex'}}>
+            <span>Delivery: </span>
+            {' '}
+            <p>35m - 1h:40m</p>
+            </div>
+            <div style={{display: 'flex'}}>
+            <span>Entrega: </span>
+            <p>A partir de R$ 3,00</p>
+</div>
+</div>
+            <img src={carImg} alt="Carrinho de compras" />
+            <span>5 Produtos no carrinho</span>
+            <p>Valor</p>
 
-            <Exemplo1>
-                <p>Avenida Rio Grande do Sul,1520,Estados | 58030-021 | João Pessoa - PB</p>
-                
-            </Exemplo1>
-        </Exemplo2>
-
-        <Exemplo3>
-            <Exemplo4>
-                <span>Delivery: </span>
-                <p>35m - 1h:40m</p>
-            </Exemplo4>
-
-            <Exemplo5>
-                <span>Entrega: </span>
-                <p>A partir de R$ 3,00</p>
-            </Exemplo5>
         
-        </Exemplo3>
-
-
-
-       
         </Container>
     )
 }
--- src/components/Summary/styles.ts
+++ src/components/Summary/styles.ts
@@ -1,32 +1,17 @@
 import styled from "styled-components";
 
 export const Container = styled.div`
-    display: flex;
+    max-width:  43.75rem;
+
+        p{
+            display: flex;
+        }
 `;
 
 export const Content = styled.div`
     display: flex;
     align-items: center;
+    /* justify-content: flex-end; */
+    /* flex-direction: column; */
 `;
 
-export const Exemplo1 = styled.div`
-    display: flex;
-    align-items: center;
-`;
-
-export const Exemplo2 = styled.div`
-    display: flex;
-    flex-direction: column;
-`;
-
-export const Exemplo3 = styled.div`
-`;
-
-export const Exemplo4 = styled.div`
-  
-`;
-
-export const Exemplo5 = styled.div`
-    display: inline-block;
-    flex-direction: column;
-`;
