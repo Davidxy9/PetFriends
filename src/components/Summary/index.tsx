28570fa7d5b453b2bb4c9e8336fc9e0f42a36dc4
Davidxy9 <davidwilliamlima@hotmail.com>
2021-06-29 16:08:50 -0300

    testes css Summary

--- src/components/Summary/index.tsx
+++ src/components/Summary/index.tsx
@@ -1,19 +1,40 @@
 import storeImg2 from '../../assets/storeImg2.png';
 import clockImg from '../../assets/clock.png';
-import { Container } from './styles';
+import { Container, Content, Exemplo1, Exemplo2, Exemplo3, Exemplo4, Exemplo5 } from './styles';
 
 export function Summary() {
     return (
         <Container>
         <img src={storeImg2} alt="Imagem da loja" />
-        <strong>Pet Friends Acessories</strong>
-        <img src={clockImg} alt="Imagem relógio" />
-        <h3>ABERTO AGORA</h3>
-        <p>Avenida Rio Grande do Sul,1520,Estados | 58030-021 | João Pessoa - PB</p>
-        <span>Delivery: </span>
-        <p>35m - 1h:40m</p>
-        <span>Entrega: </span>
-        <p>A partir de R$ 3,00</p>
+        <Exemplo2>
+            <Content>
+                <strong>Pet Friends Acessories</strong>
+                <img src={clockImg} alt="Imagem relógio" />
+                <h3>ABERTO AGORA</h3>
+            </Content>
+
+            <Exemplo1>
+                <p>Avenida Rio Grande do Sul,1520,Estados | 58030-021 | João Pessoa - PB</p>
+                
+            </Exemplo1>
+        </Exemplo2>
+
+        <Exemplo3>
+            <Exemplo4>
+                <span>Delivery: </span>
+                <p>35m - 1h:40m</p>
+            </Exemplo4>
+
+            <Exemplo5>
+                <span>Entrega: </span>
+                <p>A partir de R$ 3,00</p>
+            </Exemplo5>
+        
+        </Exemplo3>
+
+
+
+       
         </Container>
     )
 }
--- src/components/Summary/styles.ts
+++ src/components/Summary/styles.ts
@@ -1,5 +1,32 @@
 import styled from "styled-components";
 
 export const Container = styled.div`
-    
+    display: flex;
+`;
+
+export const Content = styled.div`
+    display: flex;
+    align-items: center;
+`;
+
+export const Exemplo1 = styled.div`
+    display: flex;
+    align-items: center;
+`;
+
+export const Exemplo2 = styled.div`
+    display: flex;
+    flex-direction: column;
+`;
+
+export const Exemplo3 = styled.div`
+`;
+
+export const Exemplo4 = styled.div`
+  
+`;
+
+export const Exemplo5 = styled.div`
+    display: inline-block;
+    flex-direction: column;
 `;
