b17a9dd142b2a1ad62d570249bd4badf132a50b5
Davidxy9 <davidwilliamlima@hotmail.com>
2021-06-30 18:31:52 -0300

    Mudanças na api

--- src/components/Product/index.tsx
+++ src/components/Product/index.tsx
@@ -1,14 +1,29 @@
 import { Container, Content, Separator } from "./styles"
+import Modal from 'react-modal';
 import imgTest2 from '../../assets/imageTeste2.png';
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { api } from "../../services/api";
 
+
+
 export function Product() {
+    const [isAddProductModalOpen, setIsAddProductModalOpen] = useState(false);
 
     useEffect(() => {
         api.get('products')
             .then(response => console.log(response.data))
     },[]);
+
+    function handleOpenProductModal() {
+        setIsAddProductModalOpen(true);
+    }
+
+    function handleCloseProductModal() {
+        setIsAddProductModalOpen(false);
+    }
+
+  
+
 
     return (
         <Separator>
@@ -19,10 +34,13 @@ export function Product() {
 
                 </input>
 
-
+                Sugestões do vendedor
             </Container>
             <Content>
-                <button>
+              
+                <button
+                    onClick={handleOpenProductModal}
+                >
                     <img src={imgTest2} alt="Imagem provisória" />
                     <strong>Shampoo Tradicional I Love Pet 5 Litros</strong>
                 </button>
@@ -47,6 +65,13 @@ export function Product() {
                     <strong>Shampoo Tradicional I Love Pet 5 Litros</strong>
                 </button>
 
+                <Modal 
+                    isOpen={isAddProductModalOpen}
+                    onRequestClose={handleCloseProductModal}
+                >
+                    <img src={imgTest2} alt="Imagem provisória" />
+                    <h2>Shampoo</h2>
+                </Modal>
             </Content>
         </Separator>
     );
