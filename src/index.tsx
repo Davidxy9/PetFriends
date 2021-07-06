283795c91ed399f2e1c4fa9b861ddc2f9dcee8ed
Davidxy9 <davidwilliamlima@hotmail.com>
2021-07-06 19:49:11 -0300

    dados do mirage completos

--- src/index.tsx
+++ src/index.tsx
@@ -109,46 +109,42 @@ createServer({
       return [
         {
           id: 11,
-          title: 'Mordedor de pelúcida Jacaré - Multi Squeaker',
-          img: 'https://www.petlove.com.br/images/products/230776/product/Brinquedo_Mordedor_Pel%C3%BAcia_Jacar%C3%A9_Multi_Squeaker_2555550.jpg?1602099153',
-          amount: 72.20,
-          description: '-Shampoo para cachorros de pelos claros para banhos semanais, 500ML'
+          title: 'Casinha Chalé Ecológica - Vermelha',
+          img: 'https://www.petlove.com.br/images/products/197696/product/Casinha_Chal%C3%A9_Ecol%C3%B3gica_Recriar_Pet_Vermelha_1913171_4.jpg?1556465784',
+          amount: 87.90,
+          description: '-Elaborada à base de polietileno de Baixa Densidade (PEBD), é durável e resistente, impermeável e pode ser lavada.'
         },
 
         {
           id: 12,
-          title: 'Mordedor de pelúcida Jacaré - Multi Squeaker',
-          img: 'https://www.petlove.com.br/images/products/230776/product/Brinquedo_Mordedor_Pel%C3%BAcia_Jacar%C3%A9_Multi_Squeaker_2555550.jpg?1602099153',
-          amount: 72.20,
-          off: 'De R̶$̶8̶0̶,̶2̶2̶',
-          description: '-Shampoo para cachorros de pelos claros para banhos semanais, 500ML'
+          title: 'Toca Carinha de Gato Cinza - Tamanho único',
+          img: 'https://www.petlove.com.br/images/products/236700/product/2648006_Toca_Carinha_de_Gato_Cinza_1.jpg?1621876897',
+          amount: 206.01,
+          description: '-Toca para gatos resistente, conforto térmico e acústico, protege o pet da luz.'
         },
 
         {
           id: 13,
-          title: 'Mordedor de pelúcida Jacaré - Multi Squeaker',
-          img: 'https://www.petlove.com.br/images/products/230776/product/Brinquedo_Mordedor_Pel%C3%BAcia_Jacar%C3%A9_Multi_Squeaker_2555550.jpg?1602099153',
-          amount: 72.20,
-          off: 'De R̶$̶8̶0̶,̶2̶2̶',
-          description: '-Shampoo para cachorros de pelos claros para banhos semanais, 500ML'
+          title: 'Casinha Barcelona para cães e gatos',
+          img: 'https://www.petlove.com.br/images/products/231903/product/Casinha_Barcelona_Preto_e_Cinza_para_C%C3%A3es_e_Gatos_2560138.jpg?1604521294',
+          amount: 251.82,
+          description: '-Casinha de alta durabilidade,lavável na máquina e enchimento removível.'
         },
 
         {
           id: 14,
-          title: 'Mordedor de pelúcida Jacaré - Multi Squeaker',
-          img: 'https://www.petlove.com.br/images/products/230776/product/Brinquedo_Mordedor_Pel%C3%BAcia_Jacar%C3%A9_Multi_Squeaker_2555550.jpg?1602099153',
-          amount: 72.20,
-          off: 'De R̶$̶8̶0̶,̶2̶2̶',
-          description: '-Shampoo para cachorros de pelos claros para banhos semanais, 500ML'
+          title: 'Cama Soft para Gatos - Cats Fofix ',
+          img: 'https://images-na.ssl-images-amazon.com/images/I/61Vfz9Jrh7L._AC_SL1200_.jpg',
+          amount: 78.06,
+          description: '-Garante conforto, possuindo almofada removível e ótimo acabamento.'
         },
 
         {
           id: 15,
-          title: 'Mordedor de pelúcida Jacaré - Multi Squeaker',
-          img: 'https://www.petlove.com.br/images/products/230776/product/Brinquedo_Mordedor_Pel%C3%BAcia_Jacar%C3%A9_Multi_Squeaker_2555550.jpg?1602099153',
-          amount: 72.20,
-          off: 'De R̶$̶8̶0̶,̶2̶2̶',
-          description: '-Shampoo para cachorros de pelos claros para banhos semanais, 500ML'
+          title: 'Cama Hello Pet Gato - interativa ',
+          img: 'https://www.petlove.com.br/images/products/232401/product/Cama_Hello_Pet_Gato_Interativa_3108174_3.jpg?1605720580',
+          amount: 197.91,
+          description: '-Cama fácil de limpar, tecido impermeável, ideal para gatos;'
         }
       ]
     });
@@ -165,37 +161,34 @@ createServer({
 
         {
           id: 17,
-          title: 'Mordedor de pelúcida Jacaré - Multi Squeaker',
-          img: 'https://www.petlove.com.br/images/products/230776/product/Brinquedo_Mordedor_Pel%C3%BAcia_Jacar%C3%A9_Multi_Squeaker_2555550.jpg?1602099153',
-          amount: 72.20,
-          description: '-Shampoo para cachorros de pelos claros para banhos semanais, 500ML'
+          title: 'Coleira chique para gatos - Para gatos elegantes',
+          img: 'https://www.petlove.com.br/images/products/235261/product/Coleira_Chique_para_Gatos_2637557_1.jpg?1619783994',
+          amount: 22.52,
+          description: '-Super confortável, estampas em alta definição e com fecho anti enforcamento.'
         },
 
         {
           id: 18,
-          title: 'Mordedor de pelúcida Jacaré - Multi Squeaker',
-          img: 'https://www.petlove.com.br/images/products/230776/product/Brinquedo_Mordedor_Pel%C3%BAcia_Jacar%C3%A9_Multi_Squeaker_2555550.jpg?1602099153',
-          amount: 72.20,
-          off: 'De R̶$̶8̶0̶,̶2̶2̶',
-          description: '-Shampoo para cachorros de pelos claros para banhos semanais, 500ML'
+          title: 'Coleira Fashion com Guizo para gatos',
+          img: 'https://www.petlove.com.br/images/products/234414/product/Coleira_Fashion_com_Guizo_para_Gatos_2635599.jpg?1618925025',
+          amount: 14.39,
+          description: '-Coleira resistente com design único,feita em plástico, metal e tecido.'
         },
 
         {
           id: 19,
-          title: 'Mordedor de pelúcida Jacaré - Multi Squeaker',
-          img: 'https://www.petlove.com.br/images/products/230776/product/Brinquedo_Mordedor_Pel%C3%BAcia_Jacar%C3%A9_Multi_Squeaker_2555550.jpg?1602099153',
-          amount: 72.20,
-          off: 'De R̶$̶8̶0̶,̶2̶2̶',
-          description: '-Shampoo para cachorros de pelos claros para banhos semanais, 500ML'
+          title: 'Coleira Honey para cães com silenciador de plaquinha',
+          img: 'https://www.petlove.com.br/images/products/227257/product/Coleira_Honey_para_C%C3%A3es_2537222.jpg?1597237362',
+          amount: 39.61,
+          description: '-Coleira com engate em metal soldado,sistema de segurança de 4 pontos e silenciador de plaquinha.'
         },
 
         {
           id: 20,
-          title: 'Mordedor de pelúcida Jacaré - Multi Squeaker',
-          img: 'https://www.petlove.com.br/images/products/230776/product/Brinquedo_Mordedor_Pel%C3%BAcia_Jacar%C3%A9_Multi_Squeaker_2555550.jpg?1602099153',
-          amount: 72.20,
-          off: 'De R̶$̶8̶0̶,̶2̶2̶',
-          description: '-Shampoo para cachorros de pelos claros para banhos semanais, 500ML'
+          title: 'Peitoral e Guia Toh Outdoor Laguna',
+          img: 'https://www.petlove.com.br/images/products/215501/product/Peitoral_e_Guia_Toh_Outdoor_para_C%C3%A3es_Laguna_2425815_3.jpg?1572025751',
+          amount: 66.68,
+          description: '-Produto de fácil adaptação,segurança e resistência,próprio para suportar fortes puxões.'
         }
       ]
     });
@@ -214,38 +207,35 @@ createServer({
 
         {
           id: 22,
-          title: 'Mordedor de pelúcida Jacaré - Multi Squeaker',
-          img: 'https://www.petlove.com.br/images/products/230776/product/Brinquedo_Mordedor_Pel%C3%BAcia_Jacar%C3%A9_Multi_Squeaker_2555550.jpg?1602099153',
-          amount: 72.20,
-          description: '-Shampoo para cachorros de pelos claros para banhos semanais, 500ML'
+          title: 'Osso Deliciosso Palito Fino - 450g',
+          img: 'https://www.petlove.com.br/images/products/189639/product/Osso-Deliciosso-Palito-Fino-450g---Deliciosso.jpg?1556401456',
+          amount: 42.30,
+          description: '-Contém Courinho Digerível para Cães,controla o tártaro,limpa os dentes.'
         },
 
 
         {
           id: 23,
-          title: 'Mordedor de pelúcida Jacaré - Multi Squeaker',
-          img: 'https://www.petlove.com.br/images/products/230776/product/Brinquedo_Mordedor_Pel%C3%BAcia_Jacar%C3%A9_Multi_Squeaker_2555550.jpg?1602099153',
-          amount: 72.20,
-          off: 'De R̶$̶8̶0̶,̶2̶2̶',
-          description: '-Shampoo para cachorros de pelos claros para banhos semanais, 500ML'
+          title: 'Osso XisDog Bio com Nó Simples para cães',
+          img: 'https://www.petlove.com.br/images/products/164770/product/Osso-Xis-Dog-Bio-com-No-Simples-Petsmart.jpg?1556369706',
+          amount: 13.50,
+          description: '-Delicioso sabor,aperitivo saudável e nutritivo,pode ser dado como demonstração de carinho e atenção'
         },
 
         {
           id: 24,
-          title: 'Mordedor de pelúcida Jacaré - Multi Squeaker',
-          img: 'https://www.petlove.com.br/images/products/230776/product/Brinquedo_Mordedor_Pel%C3%BAcia_Jacar%C3%A9_Multi_Squeaker_2555550.jpg?1602099153',
-          amount: 72.20,
-          off: 'De R̶$̶8̶0̶,̶2̶2̶',
-          description: '-Shampoo para cachorros de pelos claros para banhos semanais, 500ML'
+          title: 'Biscoito orgânico All Love Maça e Canela ',
+          img: 'https://www.petlove.com.br/images/products/198218/product/Biscoito_Org%C3%A2nico_All_Love_Ma%C3%A7a___Canela_para_C%C3%A3es_-_200_g_1925722.jpg?1556466646',
+          amount: 32.14,
+          description: '-Produto Natural e Orgânico(Certificado),indicado para cães,Sem agrotóxicos e sem transgênicos.'
         },
 
         {
           id: 25,
-          title: 'Mordedor de pelúcida Jacaré - Multi Squeaker',
-          img: 'https://www.petlove.com.br/images/products/230776/product/Brinquedo_Mordedor_Pel%C3%BAcia_Jacar%C3%A9_Multi_Squeaker_2555550.jpg?1602099153',
-          amount: 72.20,
-          off: 'De R̶$̶8̶0̶,̶2̶2̶',
-          description: '-Shampoo para cachorros de pelos claros para banhos semanais, 500ML'
+          title: 'Biscoito Pedigree Biscrok Para Cães Adultos,1kg',
+          img: 'https://images-na.ssl-images-amazon.com/images/I/71RZrm2ZR-L._AC_SL1500_.jpg',
+          amount: 32.14,
+          description: '-Contém deliciosos biscoitos assados e crocantes com Ômega 6 para pelos mais brilhantes.'
         },
 
       ]
